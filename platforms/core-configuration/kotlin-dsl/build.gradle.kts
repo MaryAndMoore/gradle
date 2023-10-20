269e69fd49c0beef825cfbd5d6e578346904bc97
József Bartók <jbartok@gradle.com>
2023-10-20 11:36:44 +0300

    Remove unnecessary dependency

--- platforms/core-configuration/kotlin-dsl/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl/build.gradle.kts
@@ -63,8 +63,6 @@ dependencies {
     implementation(libs.jspecify)
     implementation(libs.kotlinReflect)
 
-    implementation(libs.futureKotlin("build-tools-api"))
-
     runtimeOnly(libs.futureKotlin("build-tools-impl")) {
         isTransitive = false
     }
