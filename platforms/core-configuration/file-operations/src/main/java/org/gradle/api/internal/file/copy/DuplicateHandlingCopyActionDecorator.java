omeos-fork/gradle.gradle

Migrate Copy.destinationDir and Sync.destinationDir to lazy property

platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/DuplicateHandlingCopyActionDecorator.java
@@ -72,7 +72,7 @@ public class DuplicateHandlingCopyActionDecorator implements CopyAction {
     }
 
     private String buildFormattedOutputPath(RelativePath relativePath) {
-        return TextUtil.toPlatformLineSeparators(spec.getDestinationDir() == null ? relativePath.getPathString() : new File(spec.getDestinationDir(), relativePath.getPathString()).getPath());
+        return TextUtil.toPlatformLineSeparators(!spec.getDestinationDir().isPresent() ? relativePath.getPathString() : new File(spec.getDestinationDir().getAsFile().get(), relativePath.getPathString()).getPath());
     }
 
     private void failWithIncorrectDuplicatesStrategySetup(RelativePath relativePath) {

platforms/jvm/language-jvm/src/main/java/org/gradle/language/jvm/tasks/ProcessResources.java
@@ -31,7 +31,7 @@ public abstract class ProcessResources extends Copy {
 
     @Override
     protected void copy() {
-        boolean cleanedOutputs = StaleOutputCleaner.cleanOutputs(getDeleter(), getOutputs().getPreviousOutputFiles(), getDestinationDir());
+        boolean cleanedOutputs = StaleOutputCleaner.cleanOutputs(getDeleter(), getOutputs().getPreviousOutputFiles(), getDestinationDir().getAsFile().get());
         super.copy();
         if (cleanedOutputs) {
             setDidWork(true);

platforms/jvm/plugins-application/src/main/java/org/gradle/api/plugins/ApplicationPlugin.java
@@ -112,7 +112,7 @@ public abstract class ApplicationPlugin implements Plugin<Project> {
         @Override
         public void execute(Task task) {
             Sync sync = (Sync) task;
-            File destinationDir = sync.getDestinationDir();
+            File destinationDir = sync.getDestinationDir().getAsFile().get();
             if (destinationDir.isDirectory()) {
                 String[] children = destinationDir.list();
                 if (children == null) {

MaryAndMoore/gradle

platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/DuplicateHandlingCopyActionDecorator.java
@@ -72,7 +72,7 @@ public class DuplicateHandlingCopyActionDecorator implements CopyAction {
     }
 
     private String buildFormattedOutputPath(RelativePath relativePath) {
-        return TextUtil.toPlatformLineSeparators(spec.getDestinationDir() == null ? relativePath.getPathString() : new File(spec.getDestinationDir(), relativePath.getPathString()).getPath());
+        return TextUtil.toPlatformLineSeparators(!spec.getDestinationDir().isPresent() ? relativePath.getPathString() : new File(spec.getDestinationDir().get().getAsFile(), relativePath.getPathString()).getPath());
     }
 
     private void failWithIncorrectDuplicatesStrategySetup(RelativePath relativePath) {

platforms/jvm/language-jvm/src/main/java/org/gradle/language/jvm/tasks/ProcessResources.java
@@ -31,7 +31,7 @@ public abstract class ProcessResources extends Copy {
 
     @Override
     protected void copy() {
-        boolean cleanedOutputs = StaleOutputCleaner.cleanOutputs(getDeleter(), getOutputs().getPreviousOutputFiles(), getDestinationDir());
+        boolean cleanedOutputs = StaleOutputCleaner.cleanOutputs(getDeleter(), getOutputs().getPreviousOutputFiles(), getDestinationDir().get().getAsFile());
         super.copy();
         if (cleanedOutputs) {
             setDidWork(true);

platforms/jvm/plugins-application/src/main/java/org/gradle/api/plugins/ApplicationPlugin.java
@@ -113,7 +113,7 @@ public abstract class ApplicationPlugin implements Plugin<Project> {
         @Override
         public void execute(Task task) {
             Sync sync = (Sync) task;
-            File destinationDir = sync.getDestinationDir();
+            File destinationDir = sync.getDestinationDir().get().getAsFile();
             if (destinationDir.isDirectory()) {
                 String[] children = destinationDir.list();
                 if (children == null) {